cc00757b4cf860f62a116c3f6eebd0e52793cc36
calin <calinoiu.alexandru@agilefreaks.com>
2014-05-08 13:01:29 +0300

    updates deploy script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,6 @@ role :app, %w(deploy@46.16.191.70)
 role :web, %w(deploy@46.16.191.70)
 
 set :deploy_to, '/var/www/omnisync_staging'
-set :branch, 'master'
+set :branch, 'staging'
 
 set :puma_workers, 4
