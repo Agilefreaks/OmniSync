8c1f4cc0a9629877db489af42aecda734fc98aaa
calin <calinoiu.alexandru@agilefreaks.com>
2014-10-01 17:58:30 +0300

    updates staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :deploy_to, '/var/www/omnisync'
 set :branch, 'staging'
 
-role :app, %w(deploy@syncstaging01.omnipasteapp.com)
-role :web, %w(deploy@syncstaging01.omnipasteapp.com)
+role :app, %w(deploy@178.62.225.139)
+role :web, %w(deploy@178.62.225.139)
